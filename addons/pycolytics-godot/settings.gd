c2b9e819abfe7500d14e5114cf9fda6384f84bdf
KerekesDavid <drakediskeve@gmail.com>
2025-09-25 14:29:15 +0200

    Revert init_setting for readability
    Just a personal style choice :)

--- addons/pycolytics-godot/settings.gd
+++ addons/pycolytics-godot/settings.gd
@@ -8,9 +8,11 @@ const HASH_SALT_SETTING := &"addons/pycolytics/hash_salt"
 
 
 static func init_defaults() -> void:
-	init_setting(
-		API_KEY_SETTING,
-		DEFAULT_API_KEY,
+	if not ProjectSettings.has_setting(API_KEY_SETTING):
+		ProjectSettings.set_setting(API_KEY_SETTING, DEFAULT_API_KEY)
+	ProjectSettings.set_initial_value(API_KEY_SETTING, DEFAULT_API_KEY)
+	ProjectSettings.set_as_basic(API_KEY_SETTING, true)
+	ProjectSettings.add_property_info(
 		{
 			"name": API_KEY_SETTING,
 			"type": TYPE_STRING,
@@ -31,29 +33,19 @@ static func init_defaults() -> void:
 			"doc": "URL to a pycolytics server."
 		}
 	)
-	init_setting(
-		HASH_SALT_SETTING,
-		("%x" % randi()).left(8),
+
+	if not ProjectSettings.has_setting(HASH_SALT_SETTING):
+		ProjectSettings.set_setting(HASH_SALT_SETTING, ("%x" % randi()).left(8))
+	ProjectSettings.set_as_basic(HASH_SALT_SETTING, false)
+	ProjectSettings.add_property_info(
 		{
 			"name": HASH_SALT_SETTING,
 			"type": TYPE_STRING,
 			"hint": PROPERTY_HINT_NONE,
 			"doc": "Salt used to generate anonymised UserIDs. \
 							Changing this will make it impossible to match with UserIDs created before the change."
-		},
-		false,
-		false
+		}
 	)
-
 	var error: int = ProjectSettings.save()
 	if error:
 		push_error("Encountered error %d when saving project settings." % error)
-
-
-static func init_setting(name: String, value: Variant, property_info: Dictionary = {}, basic: bool = true, set_initial: bool = true) -> void:
-	if not ProjectSettings.has_setting(name):
-		ProjectSettings.set_setting(name, value)
-	if set_initial:
-		ProjectSettings.set_initial_value(name, value)
-	ProjectSettings.set_as_basic(name, basic)
-	ProjectSettings.add_property_info(property_info)
